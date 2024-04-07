2c31c87560724c489c8a7b8d44a9c7d002facda8
AEHTomaszRokoszynski <trokoszynski1pl@gmail.com>
2024-04-07 14:40:59 +0200

    zad3

--- pp-lab-3/src/Main.java
+++ pp-lab-3/src/Main.java
@@ -1,6 +1,25 @@
+import geometry.Circle;
+import geometry.ColoredCircle;
+import geometry.Point;
+
 public class Main {
     public static void main(String[] args) {
-        Point point = new Point(3.5, 2.0);
-        System.out.println("Współrzędne punktu: (" + point.getX() + ", " + point.getY() + ")");
+        
+        Circle[] circles = new Circle[3];
+        circles[0] = new Circle(new Point(1.0, 2.0), 3.0);
+        circles[1] = new ColoredCircle(new Point(2.0, 3.0), 4.0, "red");
+        circles[2] = new Circle(new Point(3.0, 4.0), 5.0);
+        
+      
+        // for (Circle circle : circles) {
+            
+        //     System.out.println("Area: " + circle.getArea());
+            
+           
+            if (circle instanceof ColoredCircle) {
+                ColoredCircle coloredCircle = (ColoredCircle) circle; 
+                System.out.println("Color: " + coloredCircle.getColor());
+            }
+        }
     }
 }
